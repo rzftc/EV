716c0cb3ad96873d650e20631930a293958d6350
rzftc <81071323+rzftc@users.noreply.github.com>
2025-12-04 08:15:28 +0800

    Update joint_simulation_for_test.m

--- EV/10verify/joint_simulation_for_test.m
+++ EV/10verify/joint_simulation_for_test.m
@@ -1,7 +1,14 @@
+%% 分布式资源1潜力预测
 test_DER_1;
+%% 分布式资源2潜力预测
 test_DER_2;
+%% 分布式资源3潜力预测
 test_DER_3;
+%% 分布式资源4潜力预测
 test_DER_4;
+%% 分布式资源5潜力预测
 test_DER_5;
+%% 分布式资源6潜力预测
 test_DER_6;
+%% 验证预测精确度
 verify_Joint_Potential_Accuracy;
